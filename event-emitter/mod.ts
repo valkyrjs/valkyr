af6577cbb2da4e669b8629532e91b02bff953c3c
kodemon <hi@kodemon.net>
2024-08-10 11:27:37 +0200

    feat(event-emitter): add documentation

--- event-emitter/mod.ts
+++ event-emitter/mod.ts
@@ -1,6 +1,26 @@
 import { EventEmitter as BaseEventEmitter } from "eventemitter3";
 
+/**
+ * Minimal `EventEmitter` interface that is molded against the Node.js
+ * `EventEmitter` interface.
+ */
 export class EventEmitter<EventTypes extends ValidEventTypes = string | symbol> extends BaseEventEmitter<EventTypes> {
+  /**
+   * Add a event subscriber to the given event.
+   *
+   * @param event   - Event to subscribe to.
+   * @param fn      - Event listener method to execute when event occurs.
+   * @param destroy - Method to execute when the subscription is terminated.
+   *
+   * @example
+   * ```ts
+   * const subscription = eventEmitter.subscribe("foo", () => {
+   *   // handle foo event ...
+   * }, () => {
+   *   // clean up on subscription termination ...
+   * })
+   * ```
+   */
   subscribe<T extends EventNames<EventTypes>>(
     event: T,
     fn: EventListener<EventTypes, T>,
@@ -24,7 +44,14 @@ export class EventEmitter<EventTypes extends ValidEventTypes = string | symbol> 
  |--------------------------------------------------------------------------------
  */
 
+/**
+ * EventEmitter subscription.
+ */
 export type Subscription = {
+  /**
+   * Removes event listener for the event handler that was registered with the
+   * subscriber.
+   */
   unsubscribe: () => void;
 };
 
