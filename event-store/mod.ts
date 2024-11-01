1654d548128d9d1d15878cda142cdad3297c183a
kodemon <hi@kodemon.net>
2024-11-01 18:59:32 +0100

    fix: export projector

--- event-store/mod.ts
+++ event-store/mod.ts
@@ -1,5 +1,6 @@
 export { AggregateRoot } from "./libraries/aggregate.ts";
 export * from "./libraries/errors.ts";
+export { Projector } from "./libraries/projector.ts";
 export { makeAggregateReducer, makeReducer } from "./libraries/reducer.ts";
 export * from "./libraries/time.ts";
 export { printEvents } from "./printers/printer.ts";
