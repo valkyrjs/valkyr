aa1476a265f09fcb97ddfc234eb4469badfdbbc3
kodemon <hi@kodemon.net>
2024-11-05 00:25:40 +0100

    fix(event-store): don't export fs code from root

--- event-store/mod.ts
+++ event-store/mod.ts
@@ -3,6 +3,5 @@ export * from "./libraries/errors.ts";
 export { Projector } from "./libraries/projector.ts";
 export { makeAggregateReducer, makeReducer } from "./libraries/reducer.ts";
 export * from "./libraries/time.ts";
-export { printEvents } from "./printers/printer.ts";
 export type { Empty } from "./types/common.ts";
 export type { Event, EventRecord, EventToRecord } from "./types/event.ts";
